3cebc69b40f0721c14e5b07d80714f8960dc7ad3
xhocquet <xhocquet@gmail.com>
2020-11-30 20:01:55 -0600

    Update icon based on bias score

--- source/background.js
+++ source/background.js
@@ -1,2 +1,10 @@
 // eslint-disable-next-line import/no-unassigned-import
 import './options-storage';
+
+browser.runtime.onMessage.addListener(notify);
+
+function notify(message) {
+  chrome.browserAction.setIcon({path : message.iconPath});
+}
+
+
--- source/content.js
+++ source/content.js
@@ -13,12 +13,35 @@ async function init() {
   console.log(`found ${toplevelDomain} in bias dataset`)
 
   chrome.storage.sync.get('biasMeasure', function(items) {
-    console.log('fetched storage')
-    console.log(items)
+    let newIconPath = 'gray.png'
     let newBiasMeasure = biasData.mean[toplevelDomain]
-    if (!!items.biasMeasure) {
-      newBiasMeasure += items.biasMeasure
+    if (items.biasMeasure) { newBiasMeasure = items.biasMeasure + newBiasMeasure }
+
+    switch (true) {
+      case (newBiasMeasure < -100):
+          newIconPath = '/blue-3-3.png'
+          break;
+      case (newBiasMeasure < -50):
+          newIconPath = '/blue-2-3.png'
+          break;
+      case (newBiasMeasure < -20):
+          newIconPath = '/blue-1-3.png'
+          break;
+      case (newBiasMeasure === 0):
+          newIconPath = '/gray.png'
+          break;
+      case (newBiasMeasure < 20):
+          newIconPath = '/red-1-3.png'
+          break;
+      case (newBiasMeasure < 50):
+          newIconPath = '/red-2-3.png'
+          break;
+      default:
+          newIconPath = '/red-3-3.png'
+          break;
     }
+
+    browser.runtime.sendMessage({iconPath: newIconPath})
 
     chrome.storage.sync.set({biasMeasure: newBiasMeasure}, function() {
       console.log('Settings saved with new bias measure', newBiasMeasure);
